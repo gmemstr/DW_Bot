634a2aef1e6e8cdfd487f5cf2658d54174e05b14
Donald Page <don.page.work@gmail.com>
2016-02-28 15:21:08 -0500

    ADDED: sending out points in sendPoints function inside pointsper plugin

--- server/bot/plugins/pointper.ts
+++ server/bot/plugins/pointper.ts
@@ -3,11 +3,21 @@ import * as gameService from '../../services/game.service';
 import * as twitchService from '../../services/twitch.service';
 import * as moment from 'moment';
 
+const offline: number = 10;
+const online: number = 25;
+
 export default function (bot) {
   function sendPoints() {
     twitchService.getChatters(chat => {
       let viewers = chat.chatters.viewers;
       let count = chat.chatter_count;
+
+      twitchService.getStreamStatus(status => {
+        if (status) userService.putDevbits(viewers.join(), online, () =>
+          bot.say(`Thanks for watching DevWars! You've been awarded ${online} bits.`));
+        else userService.putDevbits(viewers.join(), offline, () =>
+          bot.say(`You've earned ${offline} bits! You'll earn ${offline} bits every 15minutes whilst the stream is offline.`));
+      })
 
     })
   }
