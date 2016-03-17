c035d4cafe7a703cc7c73a8c1dccb507ae81a6ba
Donald Page <don.page.work@gmail.com>
2016-03-16 22:57:34 -0400

    CHANGED: imports in voting.ts and ADDED: changeStage function inside firebase.service

--- server/bot/plugins/voting.ts
+++ server/bot/plugins/voting.ts
@@ -1,8 +1,8 @@
-import * as firebaseService from '../../services/firebase.service';
+import {Stages, changeStage} from "../../services/firebase.service";
 
 export default function (bot) {
   bot.addCommand('@firebase', function(o) {
-    firebaseService.test();
-  })
+    changeStage(Stages.objective);
+  });
 }
 
--- server/services/firebase.service.ts
+++ server/services/firebase.service.ts
@@ -1,15 +1,20 @@
 var ref = null;
+
+export enum Stages { objective, betting, voting }
+
+
+
 export default function(firebase, authData) {
   console.log("Firebase Starting data:", authData);
   ref = firebase;
 }
 
 
-
 export function test() {
   ref.once('value', snap => console.log('snap ', snap))
 }
 
-export function getDb(cb: Function) {
-
+export function changeStage(stage: Stages, cb: Function = () => {}) {
+  const newStage = Stages[stage];
+  ref.child('frame').child('stage').set(newStage);
 }
