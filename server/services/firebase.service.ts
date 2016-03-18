9598f69f94a065290646e0fb4889a0e045dfcd29
Donald Page <don.page.work@gmail.com>
2016-03-17 21:44:26 -0400

    ADDED: addVote, changeVoteCat, and updateGame functions in firebase.service.

--- server/services/firebase.service.ts
+++ server/services/firebase.service.ts
@@ -2,6 +2,8 @@ var ref = null;
 
 export enum Stages { objective, betting, voting }
 
+export enum VoteCategories { design, func, tiebreaker }
+
 
 
 export default function(firebase, authData) {
@@ -18,3 +20,52 @@ export function changeStage(stage: Stages, cb: Function = () => {}) {
   const newStage = Stages[stage];
   ref.child('frame').child('stage').set(newStage);
 }
+
+export function addVote(color: string, category: VoteCategories, count: number = 1, cb: Function = () => {}) {
+  const cat = VoteCategories[category];
+  ref.child('frame').child('liveVoting').child(cat).child(color).transaction(currentNum => currentNum + count)
+}
+
+export function changeVoteCat(category: VoteCategories, title?: string, cb: Function = () => {}) {
+  const cat = VoteCategories[category];
+  const voteRef = ref.child('frame').child('liveVoting').child('votingOn');
+  title ? voteRef.set(`${cat}:${title}`) : voteRef.set(cat);
+}
+
+
+export function updateGame(gameData = {id: false, game: false}) {
+  //if no game data is provided it will reset the values.
+  ref.child('frame').update({
+    lastUpdated: Firebase.ServerValue.TIMESTAMP,
+    currentGameId: gameData.id,
+    timer: Firebase.ServerValue.TIMESTAMP,
+    stage: 'objective',
+    game: gameData,
+
+    liveVoting: {
+
+      votingOn: false,
+
+      design: {
+        red: 0,
+        blue: 0
+      },
+      func: {
+        red: 0,
+        blue: 0
+      },
+      tiebreaker: {
+        red: 0,
+        blue: 0
+      }
+    },
+
+    betting: {
+      red: 0,
+      blue: 0,
+      highestBetter: false
+    }
+  });
+}
+
+
