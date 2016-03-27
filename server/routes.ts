f13343f4c3ec6fe11a1bb05a47d25cd8a97438bb
Donald Page <don.page.work@gmail.com>
2016-03-27 12:03:08 -0400

    REMOVED: test route url endpoint for now

--- server/routes.ts
+++ server/routes.ts
@@ -1,9 +1,5 @@
 'use strict';
 
 export default function (app) {
-  app.use('/api/test', require('./api/test'));
-
-  app.route('/*').get((req, res) => {
-    console.log("Are you lost?");
-  })
+  // app.use('/api/test', require('./api/test'));
 }
