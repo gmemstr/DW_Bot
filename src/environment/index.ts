8548545bc14c8c6ab95023fee7427bb4288dfd09
Don Page <don.page.work@gmail.com>
2017-06-22 00:25:27 -0400

    deleted some env code in index.ts, its no longer needed. Added fallback if shared config is not present.

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,5 +1,4 @@
 /* tslint:disable */
-import * as path from 'path';
 import * as _ from 'lodash';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -7,24 +6,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // Public server configurations will extend these options
 const all = {
   env: process.env.NODE_ENV,
-
-  // Root path of server
-  root: path.normalize(__dirname + '/../../..'),
-
-  // Server port
-  port: process.env.PORT || 9000,
-
-  // Server IP
-  ip: process.env.IP || '0.0.0.0',
-
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true,
-      },
-    },
-  },
 
   bot: {
     mods: ['divine_don', 'syntag', 'b3zman41'],
@@ -43,6 +24,6 @@ const all = {
 
 export default <any>_.merge(
   all,
-  require('./shared')['shared'],
+  require('./shared')['shared'] || {},
   require(`./${process.env.NODE_ENV}`)[`${process.env.NODE_ENV}`]
 );
