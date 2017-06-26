add4e1535e11c383e389ee4cad6dc182d61f09e2
Don Page <don.page.work@gmail.com>
2017-06-25 21:43:00 -0400

    added base configuation values for betting. Started betting-spec.

--- src/plugins/betting.ts
+++ src/plugins/betting.ts
@@ -1,7 +1,42 @@
 import { ICommandPayload, TwitchBot } from '../bot';
+import * as moment from 'moment';
+
+export interface IBetter {
+  name: string;
+  tier: number;
+  team: string;
+  amount: number;
+  winnings: number;
+}
+
+interface IPool {
+  open: boolean;
+  openTime: number;
+  gameId: number;
+  bets: [IBetter];
+}
+
+enum Teams {
+  red,
+  blue,
+}
+
+enum BettingTiers {
+  tie = 0, one,
+  two, three,
+  four, five,
+}
+
+enum betVariation { type1, type2, type3 }
+
+const maxBet = 10;
+const minBet = 10000;
+
+const bettingDuration = moment.duration(10, 'minutes');
+
 export class BettingPlugin {
   constructor(bot: TwitchBot) {
-    bot.addCommand('*testBet', (o: ICommandPayload) => {
+    bot.addCommand('*testBet', (p: ICommandPayload) => {
       bot.say('test bet command');
     });
   }
---
+++ test/betting-spec.js
@@ -0,0 +1,23 @@
+import test from 'ava';
+import environment from '../lib/environment'
+import { TwitchBot } from '../lib/bot';
+const bot = new TwitchBot(environment.bot);
+
+/*
+ * betting docs:
+ * https://docs.google.com/spreadsheets/d/1kg4zsgf2d_FqbshCdTii3oZnU_W5Imlj_ka6sRfaiCk/edit#gid=2069817316
+ */
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const ghostBetters = [
+  {name: 'Gastly', tier: 3, team: 'blue', amount: 300, winnings: 0},
+  {name: 'Haunter', tier: 4, team: 'tie', amount: 300, winnings: 0},
+  {name: 'Gengar', tier: 5, team: 'red', amount: 300, winnings: 0},
+];
+
+test('', t => {
+  t.true(true);
+});
