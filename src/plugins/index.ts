9fede6adba13481100506533a9bcce3f3a0280d4
Don Page <don.page.work@gmail.com>
2017-07-18 23:32:48 -0400

    added(user.service): get, put, and has bits endpoints and helpers.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,13 +1,16 @@
 import { IPayload, TwitchBot } from '../bot';
 import { BettingPlugin } from './betting';
-import { getDevBits } from '../services/user.service';
+import { getBits } from '../services/user.service';
 
 const plugins = (bot: TwitchBot) => {
   new BettingPlugin(bot);
   bot.addCommand('*devbits', async (o:IPayload) => {
-    console.log(`o:`);
-    console.log(o);
-    const bits = await getDevBits(o.user.username);
+    const bits = await getBits(o.user.username);
+    return bot.say(`${o.user.username}: ${bits}`);
+  });
+
+  bot.addCommand('*bits', async (o:IPayload) => {
+    const bits = await getBits(o.user.username);
     return bot.say(`${o.user.username}: ${bits}`);
   });
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,7 +4,19 @@ import environment from '../environment';
 const url = environment.dwServer.url;
 const key = environment.dwServer.key;
 
-export async function getDevBits(username: string): Promise<number> {
-  const req = await axios(`${url}/v1/devbits/${username}`);
+export async function getBits(user: string): Promise<number> {
+  const req = await axios(`${url}/v1/devbits/${user}`);
   return Number(req.data);
+}
+
+export async function putBits(user: string, amount: number): Promise<void> {
+  try {
+    await axios.put(`${url}/v1/devbits/${user}/${amount}/?key=${key}`);
+    return;
+  } catch (e) { console.log(e); }
+}
+
+export async function hasBits(user: string, amount: number): Promise<boolean> {
+  const dbAmount = await getBits(user);
+  return dbAmount > amount;
 }
