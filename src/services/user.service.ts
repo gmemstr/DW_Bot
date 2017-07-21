b8bd32d644e8838bf3f9331a89e44d82d4c3bac6
Don Page <don.page.work@gmail.com>
2017-07-20 21:44:40 -0400

    refactor(user.service): changed user param to name. User was confusing because of IUser.

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,19 +4,19 @@ import environment from '../environment';
 const url = environment.dwServer.url;
 const key = environment.dwServer.key;
 
-export async function getBits(user: string): Promise<number> {
-  const req = await axios(`${url}/v1/devbits/${user}`);
+export async function getBits(name: string): Promise<number> {
+  const req = await axios(`${url}/v1/devbits/${name}`);
   return Number(req.data);
 }
 
-export async function putBits(user: string, amount: number): Promise<void> {
+export async function putBits(name: string, amount: number): Promise<void> {
   try {
-    await axios.put(`${url}/v1/devbits/${user}/${amount}/?key=${key}`);
+    await axios.put(`${url}/v1/devbits/${name}/${amount}/?key=${key}`);
     return;
   } catch (e) { console.log(e); }
 }
 
-export async function hasBits(user: string, amount: number): Promise<boolean> {
-  const dbAmount = await getBits(user);
+export async function hasBits(name: string, amount: number): Promise<boolean> {
+  const dbAmount = await getBits(name);
   return dbAmount > amount;
 }
