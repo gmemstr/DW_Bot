9406ece84b3e209dd8fdfd653fc489cc2d3612bd
DonPage <don.page.work@gmail.com>
2018-02-04 17:41:10 -0800

    changed bpm numbers

--- src/plugins/bpm.plugin.ts
+++ src/plugins/bpm.plugin.ts
@@ -7,7 +7,7 @@ import * as moment from 'moment';
 export class BPMPlugin {
   private offline = 2;
   private online = 10;
-  private loop: number = this.ms(30, 'minutes');
+  private loop: number = this.ms(25, 'minutes');
 
   constructor(private bot: TwitchBot) {
     bot.addCommand('@bpm', async () => {
@@ -22,7 +22,6 @@ export class BPMPlugin {
   }
 
   private async giveBits() {
-    console.log(`giveBits()`);
     // get everyone's usernames that are in chat.
     const viewers = await getViewers();
     if (viewers) {
